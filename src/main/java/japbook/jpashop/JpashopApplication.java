f7e67d6afd912a5e6c689314522ae9fb6f49c965
OhYongUn <dhdyddjs@naver.com>
2023-07-24 18:37:38 +0900

    주문도메인 및 동적쿼리에관한 예제..

--- src/main/java/japbook/jpashop/JpashopApplication.java
+++ src/main/java/japbook/jpashop/JpashopApplication.java
@@ -3,11 +3,14 @@ package japbook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
+
 	}
 
 }
